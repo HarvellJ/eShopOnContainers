93849c15866b67f9a41a686405221d134ebaf841
HarvellJ <40071640+HarvellJ@users.noreply.github.com>
2021-08-27 09:23:04 +0100

    Added TF files for creating SLO alerts.

---
+++ deploy/azure/main.tf
@@ -0,0 +1,50 @@
+# Configure the Azure provider
+terraform {
+    required_providers {
+        azurerm = {
+                source  = "hashicorp/azurerm"
+                version = ">= 2.59"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+#Deploy a sample log query alert
+resource "azurerm_monitor_scheduled_query_rules_alert" "SLO_ALERT" {
+    for_each            = var.SLOs
+    name                = format("%s-%s%s", each.value["userJourneyCategory"], each.value["sloCategory"], 
+                            each.value["sloPercentile"])
+    location            = var.resourceRegion
+    resource_group_name = var.logAnalyticsResourceGroupName
+
+    action {
+        action_group           = var.alertActionGroups
+        email_subject          = format("Alert - SLO Breach: %s-%s%s", each.value["userJourneyCategory"], 
+                                    each.value["sloCategory"], each.value["sloPercentile"])
+        custom_webhook_payload = var.webHookPayLoad
+    }
+
+    data_source_id = var.logAnalyticsResourceID
+    description    = each.value["sloDescription"]
+    enabled        = true
+    query          = <<-QUERY
+        ${each.value["signalQuery"]}
+    QUERY
+    
+    severity    = each.value["signalSeverity"]
+    frequency   = each.value["frequency"]
+    time_window = each.value["time_window"]  
+    trigger {
+        operator  = each.value["triggerOperator"]
+        threshold = each.value["triggerThreshold"]
+        metric_trigger {
+            operator  = "GreaterThan"
+            threshold = 1
+            metric_trigger_type = "Total"
+            metric_column = "AggregatedValue"
+        }
+    }
+}
